fba634ab65b1af52acd07089f494fdb867dc2377
WithLithum <WithLithum@outlook.com>
2025-12-14 16:19:37 +0800

    ✅ Add testing for ProfileOrchestrator
    Signed-off-by: WithLithum <WithLithum@outlook.com>

---
+++ MineOrc.Tests/ProfileOrchestratorTests.cs
@@ -0,0 +1,87 @@
+﻿// SPDX-FileCopyrightText: 2025 WithLithum & contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+using MineOrc.Foundation.Instancing;
+using MineOrc.Tests.Utils;
+
+namespace MineOrc.Tests;
+
+public class ProfileOrchestratorTests
+{
+    [Fact]
+    public void GetMainClass_NoExtensions_ReturnsManifestMainClass()
+    {
+        // Arrange
+        var profile = new ProfileInfo
+        {
+            ClientVersion = "MockVersion",
+            Created = DateTime.MinValue,
+            Extensions = null,
+        };
+
+        // Act
+        var result = ProfileOrchestrator.GetMainClass(FakeData.ExampleVersion,
+            profile);
+
+        // Assert
+        Assert.Equal(FakeData.ExampleMainClass, result);
+    }
+
+    [Fact]
+    public void GetMainClass_SingleExtension_ReturnsExtensionMainClass()
+    {
+        // Arrange
+        var profile = new ProfileInfo
+        {
+            ClientVersion = "MockVersion",
+            Created = DateTime.MinValue,
+            Extensions =
+            [
+                new ProfileExtension
+                {
+                    MainClass = "org.example.extension.ProfileClient",
+                },
+            ],
+        };
+
+        // Act
+        var result = ProfileOrchestrator.GetMainClass(FakeData.ExampleVersion,
+            profile);
+
+        // Assert
+        Assert.Equal("org.example.extension.ProfileClient", result);
+    }
+    
+    [Fact]
+    public void GetMainClass_MultipleExtensions_ReturnsLastExtensionMainClass()
+    {
+        // Arrange
+        var profile = new ProfileInfo
+        {
+            ClientVersion = "MockVersion",
+            Created = DateTime.MinValue,
+            Extensions =
+            [
+                new ProfileExtension
+                {
+                    MainClass = "org.example.extension.ProfileClient",
+                },
+                new ProfileExtension
+                {
+                    MainClass = "org.example.extension.SomeOtherExtension",
+                },
+                new ProfileExtension
+                {
+                    MainClass = "com.example.extension.YetAnotherExtensionClient",
+                },
+            ],
+        };
+
+        // Act
+        var result = ProfileOrchestrator.GetMainClass(FakeData.ExampleVersion,
+            profile);
+
+        // Assert
+        Assert.Equal("com.example.extension.YetAnotherExtensionClient", result);
+    }
+}
---
+++ MineOrc.Tests/Utils/FakeData.cs
@@ -0,0 +1,41 @@
+﻿// SPDX-FileCopyrightText: 2025 WithLithum & contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+using System.Collections.Frozen;
+using MineOrc.Foundation.Manifest;
+using MineOrc.Foundation.Manifest.Network;
+using MineOrc.Foundation.Manifest.Options;
+
+namespace MineOrc.Tests.Utils;
+
+public static class FakeData
+{
+    public static readonly AssetIndexArtefactInfo AssetIndexArtefact = new()
+    {
+        Id = "",
+        Sha1 = "",
+        Size = 0,
+        Url = new Uri("https://contoso.com"),
+        TotalSize = 0,
+    };
+
+    public static readonly VersionArguments EmptyVersionArguments = new()
+    {
+        Game = [],
+        Jvm = [],
+    };
+
+    public const string ExampleMainClass = "com.contoso.example.MineOrcExample";
+    
+    public static readonly ClientManifest ExampleVersion = new()
+    {
+        MainClass = ExampleMainClass,
+        Libraries = [],
+        Id = "MockVersion",
+        Arguments = EmptyVersionArguments,
+        AssetIndex = AssetIndexArtefact,
+        Downloads = FrozenDictionary<string, ArtefactInfo>.Empty,
+        Assets = "",
+        ComplianceLevel = 2,
+    };
+}
